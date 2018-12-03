71faf9e57af91b41fe1c1ebeb68791ca41206bfe
Jack <jjy_8888@163.com>
2018-12-03 18:04:42 +0800

    add unit test

--- src/library/util/src/main/java/com/jack/LoadResourceTest.java
+++ src/library/util/src/main/java/com/jack/LoadResourceTest.java
@@ -1,12 +1,14 @@
 package com.jack;
 
-
+import java.io.File;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
 import java.util.Properties;
 
 public class LoadResourceTest {
-    public static void main(String[] args) throws Exception{
-        System.out.println(LoadResourceTest.class.getResource(""));
+    public static void main(String[] args) throws Exception {
+        System.out.println(LoadResourceTest.class.getResource("/com/jack/User.class"));
         System.out.println(LoadResourceTest.class.getResource("/"));
 
         System.out.println(LoadResourceTest.class.getClassLoader().getResource(""));
@@ -16,5 +18,16 @@ public class LoadResourceTest {
         Properties properties = new Properties();
         properties.load(in);
         System.out.println(properties.getProperty("jdbc.driver"));
+
+        System.out.println("print package class....");
+        Enumeration<URL> urls = Thread.currentThread().getContextClassLoader().getResources("com/jack");
+        while (urls.hasMoreElements()) {
+            URL url = urls.nextElement();
+            File[] files = new File(url.getPath()).listFiles(pathname -> (pathname.isFile() && pathname.getName().endsWith(".class") || pathname.isDirectory()));
+
+            for (File file : files) {
+                System.out.println(file.getName());
+            }
+        }
     }
 }
