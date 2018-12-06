e36345df4af54ec23bdec2bbe4c814ccfd48dbf1
jack <jjy_8888@163.com>
2018-12-07 00:11:29 +0800

    add chatper3 code

---
+++ src/architecture_explore/chapter3/smart-framework/src/test/java/org/smart4j/test/ClassUtilTest.java
@@ -0,0 +1,21 @@
+package org.smart4j.test;
+
+import org.junit.Test;
+import org.smart4j.framework.util.ClassUtil;
+
+import java.util.Set;
+
+import static org.junit.Assert.assertFalse;
+
+public class ClassUtilTest {
+
+    @Test
+    public void getClassSetTest(){
+        Set<Class<?>> set = ClassUtil.getClassSet("org.smart4j.framework");
+        assertFalse(set.isEmpty());
+
+        for(Class c : set){
+            System.out.println(c.getCanonicalName());
+        }
+    }
+}
