04a3bde60616af8fd42d431e2b7024ddc8b5cb18
Jack <jjy_8888@163.com>
2018-12-07 18:07:51 +0800

    add chatper3 code

---
+++ src/architecture_explore/chapter3/smart-framework/src/main/java/org/smart4j/framework/helper/ControllerHelper.java
@@ -0,0 +1,20 @@
+package org.smart4j.framework.helper;
+
+import org.smart4j.framework.bean.Handler;
+import org.smart4j.framework.bean.Request;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author jack
+ * @since 1.0.0
+ * */
+public final class ControllerHelper {
+
+    private static final Map<Request, Handler> ACTION_MAP = new HashMap<>();
+
+    static {
+
+    }
+}
