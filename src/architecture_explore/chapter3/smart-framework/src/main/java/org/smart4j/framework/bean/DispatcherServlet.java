e26a966669ddad5403b49a07a440a38eb2208cd4
jack <jjy_8888@163.com>
2018-12-08 13:50:16 +0800

    add chapter3 code

--- src/architecture_explore/chapter3/smart-framework/src/main/java/org/smart4j/framework/bean/DispatcherServlet.java
+++ src/architecture_explore/chapter3/smart-framework/src/main/java/org/smart4j/framework/bean/DispatcherServlet.java
@@ -1,21 +1,56 @@
 package org.smart4j.framework.bean;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServlet;
-import java.io.IOException;
+import org.smart4j.framework.helper.BeanHelper;
+import org.smart4j.framework.helper.ConfigHelper;
+import org.smart4j.framework.helper.ControllerHelper;
+import org.smart4j.framework.helper.HelperLoader;
 
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(urlPatterns = "/*", loadOnStartup = 0)
 public class DispatcherServlet extends HttpServlet {
 
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        super.service(req, res);
+    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String requestMethod = req.getMethod();
+        String requestPath = req.getPathInfo();
+
+        Handler handler = ControllerHelper.getHandler(requestMethod, requestPath);
+        if (handler != null) {
+            Class<?> controllerClass = handler.getControllerClass();
+            Object controllerBean = BeanHelper.getBean(controllerClass);
+
+            Map<String, Object> paramMap = this.getParamMap(req);
+        }
+    }
+
+    private  Map<String, Object> getParamMap(HttpServletRequest req){
+        Map<String, Object> paramMap = new HashMap<String, Object>();
+        Enumeration<String> paramNames = req.getParameterNames();
+        while (paramNames.hasMoreElements()) {
+            String paramName = paramNames.nextElement();
+            String paramValue = req.getParameter(paramName);
+            paramMap.put(paramName, paramValue);
+        }
+
+        return paramMap;
     }
 
     @Override
     public void init(ServletConfig config) throws ServletException {
-        super.init(config);
+        HelperLoader.init();
+        ServletContext servletContext = config.getServletContext();
+        ServletRegistration jspServlet = servletContext.getServletRegistration("jsp");
+        jspServlet.addMapping(ConfigHelper.getAppJspPath() + "*");
+        ServletRegistration defaultServlet = servletContext.getServletRegistration("default");
+        defaultServlet.addMapping(ConfigHelper.getAppAssetPath() + "*");
     }
 }
